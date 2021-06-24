bd352edee96d3931392bf355a8292fcc4942cff6
victorc1999 <victor.de.f.costa@gmail.com>
2021-06-24 00:08:14 +0000

    App Python v1.0

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello World\n Victor")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def pagina_inicial():
+    return "Hello World\n Victor"
+
+if __name__ == '__main__':
+    app.run(debug=True)
